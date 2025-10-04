2fda9f75c2dd597b06db2e983bd0d90a9c43faa2
dapitdh <dapitharianto1912@gmail.com>
2025-10-04 11:06:20 +0700

    Commit No.3 - Deleting an Item from RecyclerView by

--- app/src/main/java/com/example/lab_week_06/CatAdapter.kt
+++ app/src/main/java/com/example/lab_week_06/CatAdapter.kt
@@ -2,33 +2,76 @@ package com.example.lab_week_06
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.example.lab_week_06.model.CatModel
 
-class CatAdapter(private val layoutInflater: LayoutInflater, private val imageLoader: ImageLoader, private val onClickListener:
-OnClickListener) : RecyclerView.Adapter<CatViewHolder>() {
-    //Mutable list for storing all the list data
+class CatAdapter(
+    private val layoutInflater: LayoutInflater,
+    private val imageLoader: ImageLoader,
+    private val onClickListener: OnClickListener
+) : RecyclerView.Adapter<CatViewHolder>() {
+
+    val swipeToDeleteCallback = SwipeToDeleteCallback()
+
     private val cats = mutableListOf<CatModel>()
-    //A function to set the mutable list
+
     fun setData(newCats: List<CatModel>) {
         cats.clear()
         cats.addAll(newCats)
         notifyDataSetChanged()
     }
-    //A view holder is used to bind the data to the layout views
-//onCreateViewHolder is instantiating the view holder it self
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
-            CatViewHolder {
-        val view = layoutInflater.inflate(R.layout.item_list, parent, false)
-        return CatViewHolder(view, imageLoader, onClickListener as CatViewHolder.OnClickListener)
+
+    fun removeItem(position: Int) {
+        cats.removeAt(position)
+        notifyItemRemoved(position)
     }
-    override fun getItemCount() = cats.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CatViewHolder {
+        val view = layoutInflater.inflate(R.layout.item_list, parent, false)
+        return CatViewHolder(view, imageLoader, onClickListener)
+    }
+
+    override fun getItemCount(): Int = cats.size
+
     override fun onBindViewHolder(holder: CatViewHolder, position: Int) {
         holder.bindData(cats[position])
     }
-    //Declare an onClickListener interface
+
     interface OnClickListener {
         fun onItemClick(cat: CatModel)
     }
-}
 
+    inner class SwipeToDeleteCallback : ItemTouchHelper.SimpleCallback(
+        0,
+        ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+    ) {
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder
+        ): Boolean = false
+
+        override fun getMovementFlags(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder
+        ): Int {
+            return if (viewHolder is CatViewHolder) {
+                makeMovementFlags(
+                    ItemTouchHelper.ACTION_STATE_IDLE,
+                    ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+                ) or makeMovementFlags(
+                    ItemTouchHelper.ACTION_STATE_SWIPE,
+                    ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+                )
+            } else {
+                0
+            }
+        }
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            val position = viewHolder.adapterPosition
+            removeItem(position)
+        }
+    }
+}
